283b2c60a602dd2fd78d649ab01e5614e236c7f5
Evhenii <kolomoycev81@gmail.com>
2023-07-29 23:41:04 +0300

    promises

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,4 +1,4 @@
-refs = {
+const refs = {
   startBtn: document.querySelector('button[data-start]'),
   stopBtn: document.querySelector('button[data-stop]'),
   bodyEl: document.querySelector('body'),
